3f5e2266f92ce05d7ddf4b1233537526e8dd55c3
Alexey Gusev <AlekseyGusev24@gmail.com>
2021-09-19 20:14:48 +0300

    build

---
+++ src/main/java/com/example/baf/BafApplication.java
@@ -0,0 +1,13 @@
+package com.example.baf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baf/BafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
